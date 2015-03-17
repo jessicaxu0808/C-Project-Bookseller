b2f77a087240ae6f5babf52f53a9cf11efd4554e
lakv <lakv@users.noreply.github.com>
2015-03-16 17:55:10 -0700

    Update ReportModule.h

--- FinalExamProject/FinalExamProject/ReportModule.h
+++ FinalExamProject/FinalExamProject/ReportModule.h
@@ -1,3 +1,39 @@
+/*CIS 22B
+Serendipity BookSeller Program.
+Team: Aaron, Jessica, Lakshmi, Pooja.
+Author: Pooja Prabhuswamy
+Date: 03/19/2015
+Description:Serendipity Booksellers is a small bookstore located in a shopping mall. They have a
+cashier station equipped with a personal computer. The manager wants you to develop a
+point-of-sale (POS) software package that will make the computer function as a cash
+register and keep an inventory file. The inventory file will be a database of all the books
+in the bookstore. In general, the software is to perform the following tasks:
+• Calculate the total of a sale, including sales tax
+• When a book is purchased, subtract it from the inventory file
+• Add, change, delete, and look up books in the inventory file
+• Display various sales reports
+
+***********************PSUEDO CODE**********************************
+1. Class ReportModule is declared. It inherits from StoreModule class.
+2. Private constant static variables are declared. These variables choose how the elemets in booklist are sorted.
+const static int dInv = 1
+dWSVal = 2,
+dRVal = 3,
+lQT = 4,
+lWSVAL = 5,
+lDT = 6;
+
+3. It has public members : 
+ReportModule overloaded constructor
+act() function of type void
+displayInventory() function of type string
+displayWholeSaleValue() function of type string
+displayByRetailValue() function of type string
+listByQuantity() function of type string
+listByCost() function of type string
+listByDate() function of type string
+*/
+
 #ifndef REPORT_MODULE
 #define REPORT_MODULE
 
