e71496fc60d73733363348377ee8c62a09634116
lakv <lakv@users.noreply.github.com>
2015-03-19 10:39:29 -0700

    Update Book.cpp

--- FinalExamProject/FinalExamProject/Book.cpp
+++ FinalExamProject/FinalExamProject/Book.cpp
@@ -8,22 +8,20 @@ cashier station equipped with a personal computer. The manager wants you to deve
 point-of-sale (POS) software package that will make the computer function as a cash
 register and keep an inventory file. The inventory file will be a database of all the books
 in the bookstore. In general, the software is to perform the following tasks:
-
-• Calculate the total of a sale, including sales tax
-• When a book is purchased, subtract it from the inventory file
-• Add, change, delete, and look up books in the inventory file
-• Display various sales reports
-
+â€¢ Calculate the total of a sale, including sales tax
+â€¢ When a book is purchased, subtract it from the inventory file
+â€¢ Add, change, delete, and look up books in the inventory file
+â€¢ Display various sales reports
 Psuedocode:
 1. The Book constructors are defined here. In first constructor all the values of variables are set to default values. ISBN = "",bookTitle = "",
-	authorName = "", publisher = "", date, quantity = 0, wholesaleCost = 0, retailCost = 0.
+authorName = "", publisher = "", date, quantity = 0, wholesaleCost = 0, retailCost = 0.
 2. The second constructor takes in the value of string ISBN, string b, string a, string p, int day, int month, int year, int q, double c, double r
-	as input and sets them to ISBN, bookTitle, authorName, publisher, date, quantity, wholesaleCost, retailCost.
-3. toString function has multiple if else condition where it checks if the info value which is the input for this function and the values 
-	of ISB, BKTTLE, AUTHNAME, PUB, DATE, QUANT, WCOST, RET is != 0 in each case and then sets the value of string s to respective values of 
-	ISBN, Book title, Author, Publisher, date, Wholesale Cost, Retail Cost in the form of string. s retruned at the end of this function.
+as input and sets them to ISBN, bookTitle, authorName, publisher, date, quantity, wholesaleCost, retailCost.
+3. toString function has multiple if else condition where it checks if the info value which is the input for this function and the values
+of ISB, BKTTLE, AUTHNAME, PUB, DATE, QUANT, WCOST, RET is != 0 in each case and then sets the value of string s to respective values of
+ISBN, Book title, Author, Publisher, date, Wholesale Cost, Retail Cost in the form of string. s retruned at the end of this function.
 4. upperCase function takes a string input. for each character of the string the funtion toUpper is called and the character is set to upper case
-	and the string is returned.
+and the string is returned.
 5. help function takes a boolean reference variable b. If !b it sets b to 1 and returns null value. Else it returns "\n" value.
 6. setISBN takes a string input. This input is set to value of ISBN.
 7. setBookTitle takes a string input. This input is set to value of bookTitle.
@@ -43,11 +41,11 @@ Psuedocode:
 21. getWholesaleCost returns value of wholesaleCost.
 22. getRetailCost returns value of retailCost.
 23. getProperty checks if input infor and Book class's ISB, BKTITLE, AUTHNAME, PUB is != 0 and then returns ISBN, bookTitle in uppercase,
-	authorName and publisher.
+authorName and publisher.
 24. Opertor << is overloaded using reference variables ofstream out and Book b. It outputs b.ISBN, b.bookTitle, b.authorName, b.publisher,
-	b.date, b.quantity, b.wholesaleCost, b.retailCost along with \n.
-25. Operator >> is overloaded using the reference variables ifstream in and Book b. It takes in the input of 500 characters and stores it into 
-	b.ISBN, b.bookTitle, b.authorName, b.publisher,	b.date, b.quantity, b.wholesaleCost, b.retailCost.
+b.date, b.quantity, b.wholesaleCost, b.retailCost along with \n.
+25. Operator >> is overloaded using the reference variables ifstream in and Book b. It takes in the input of 500 characters and stores it into
+b.ISBN, b.bookTitle, b.authorName, b.publisher,	b.date, b.quantity, b.wholesaleCost, b.retailCost.
 */
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -76,15 +74,13 @@ ISBN(ISBN), bookTitle(b), authorName(a), publisher(p), date(day, month, year), q
 {}
 
 
-
-//SHOULD WE ADD A NEW LINE IN FRONT OF ISBN?
 string Book::toString(int info)
 {
 	string s;
 	bool flag = 0;
 	int inc = 0;
 	if ((info & Book::ISB) != 0)  // 1
-		s += help(flag) + "ISNB: " + ISBN;
+		s += help(flag) + "ISBN: " + ISBN;
 	if ((info & Book::BKTTLE) != 0)  // 2
 		s += help(flag) + "Title: " + bookTitle;
 	if ((info & Book::AUTHNAME) != 0)  // 4
@@ -230,21 +226,18 @@ string Book::getProperty(int info)
 		return publisher;
 
 	//right now, the code can only handle getters of properties that are strings
-	//the code really shouldnt reach here
 	return "";
 }
 
 
 ofstream& operator<<(ofstream& out, Book& b)
 {
-	//PUT ENDLINE INSTEAD?
 	const char endL = '\n';
 	out << b.ISBN << endL;
 	out << b.bookTitle << endL;
 	out << b.authorName << endL;
 	out << b.publisher << endL;
-	out << b.date.getMonth() << ' ' << b.date.getDay() << ' ' << b.date.getYear() << endL;   //ONCE THE PROGRAM IS WORKING, MAKE A << OPERATOR FOR DATE
-	out << b.quantity << endL;
+	out << b.date.getMonth() << ' ' << b.date.getDay() << ' ' << b.date.getYear() << endL;   
 	out << b.wholesaleCost << endL;
 	out << b.retailCost;
 	return out;
