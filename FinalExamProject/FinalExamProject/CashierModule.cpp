9ec6be1f46e255b05c71590fee2240b63b50a7e1
jessicaxu0808 <jessicaxu0808@gmail.com>
2015-03-18 19:06:48 -0700

    Update CashierModule.cpp

--- FinalExamProject/FinalExamProject/CashierModule.cpp
+++ FinalExamProject/FinalExamProject/CashierModule.cpp
@@ -14,29 +14,29 @@ Pseudocode:
 1. CashierModule inherits BookList pointer b from StoreModule, which opens the inventory file from Booklist.h
 2. The constructor sets 'items', which tracks how many books are in the checkout queue, to 0.
 3. isbnCheckOut allows the user to input up to 500 books and choose to either checkout or cancel the order.
-	-Begins loop for multiple consecutive checkouts
-		-Begins loop for multiple book purchases each time the checkout loop is run
-			-Asks user for the ISBN of the book being purchased
-			-Searches booklist for a matching ISBN or partial ISBN
-			-If exactly one match is found, checks if quantity in stock > 0
-				-If so, add the book to the checkout queue, deduct one from inventory quantity, and store book info in titles[] and prices[] for receipt. Increment items by 1.
-				-If not, output a message that the book is out of stock
-			-If more than one match is found, output a list of all partial matches and allow user to choose
-				-Loop to check for a valid choice from the list
-				-Check if the quantity of the selected book > 0
-					-If so, add the book to the checkout queue, deduct one from inventory quantity, and store book info in titles[] and prices[] for receipt Increment items by 1.
-					-If not, output a message that the book is out of stock
-			-In every loop, check if the word 'Checkout' or 'checkout' is inputted
-				-If so, break loop and proceed to checkout
-					-Calculate amd output subtotal by totalling all elements in prices[]
-					-Reset items to zero
-					-Calculate and output sales tax
-					-Calculate and output total
-					-Ask user if they wish to print a receipt
-			-In every loop, check if the word 'Exit' or 'exit' is inputted
-				-If so, break loop and return to checkout menu
-				-Restore all inventory quantities to original amounts before that iteration of the checkout loop
-				-Reset items to zero
+-Begins loop for multiple consecutive checkouts
+-Begins loop for multiple book purchases each time the checkout loop is run
+-Asks user for the ISBN of the book being purchased
+-Searches booklist for a matching ISBN or partial ISBN
+-If exactly one match is found, checks if quantity in stock > 0
+-If so, add the book to the checkout queue, deduct one from inventory quantity, and store book info in titles[] and prices[] for receipt. Increment items by 1.
+-If not, output a message that the book is out of stock
+-If more than one match is found, output a list of all partial matches and allow user to choose
+-Loop to check for a valid choice from the list
+-Check if the quantity of the selected book > 0
+-If so, add the book to the checkout queue, deduct one from inventory quantity, and store book info in titles[] and prices[] for receipt Increment items by 1.
+-If not, output a message that the book is out of stock
+-In every loop, check if the word 'Checkout' or 'checkout' is inputted
+-If so, break loop and proceed to checkout
+-Calculate amd output subtotal by totalling all elements in prices[]
+-Reset items to zero
+-Calculate and output sales tax
+-Calculate and output total
+-Ask user if they wish to print a receipt
+-In every loop, check if the word 'Exit' or 'exit' is inputted
+-If so, break loop and return to checkout menu
+-Restore all inventory quantities to original amounts before that iteration of the checkout loop
+-Reset items to zero
 4. titleCheckOut performs the same function as isbnCheckOut, except it searches books by title or partial title instead of isbn
 5. printReceipt uses the titles[] and prices[] array to output a list off all books purchased in that iteration of the checkout loop, along with the subtotal, tax, and total
 */
@@ -84,7 +84,7 @@ void CashierModule::isbnCheckOut(){
 
 			if (bookName == "exit" || bookName == "Exit"){
 				for (int i = 0; i < items; i++){		//loop resets bookList to the previous quantities of each book because checkout was cancelled
-					bookList->partialFindBooks(2, titles[i], tempV);	
+					bookList->partialFindBooks(2, titles[i], tempV);
 					if (tempV.size() == 1){ //searches for matches for the books and restores the deducted quantities when a match is found
 						tempV[0]->setQuantity(tempV[0]->getQuantity() + 1);
 					}
@@ -101,7 +101,7 @@ void CashierModule::isbnCheckOut(){
 					prices[items] = tempV[0]->getRetailCost();	//stores prices for receipt
 					subTotal += tempV[0]->getRetailCost();		//increments subtotal
 					items++;	//counter items increases by one
-					cout << endl << "$" << fixed << setprecision(2) << prices[items-1] << "     " << titles[items-1] << endl << endl; //Outputs book name and price for confirmation
+					cout << endl << "$" << fixed << setprecision(2) << prices[items - 1] << "     " << titles[items - 1] << endl << endl; //Outputs book name and price for confirmation
 				}
 				else{
 					cout << "Out of Stock" << endl << endl;
@@ -117,14 +117,13 @@ void CashierModule::isbnCheckOut(){
 				while (!readInt(answ) || (answ < 1) || (answ >tempV.size())) { //checks if user selection is valid. If not, user must re-input selection
 					cout << "\nPlease enter a number between 1 and " << tempV.size() << ": ";
 				}
-				if (tempV[0]->getQuantity() > 0){ //checks if the book is in stock
-					tempV[0]->setQuantity(tempV[0]->getQuantity() - 1); //Reduces stock by one
-					titles[items] = tempV[0]->getBookTitle(); //stores titles for receipt
-					prices[items] = tempV[0]->getRetailCost(); //stores prices for receipt
-					stock[items] = tempV[0]->getQuantity(); //increments subtotal
-					subTotal += tempV[0]->getRetailCost(); //counter items increase by one
+				if (tempV[answ - 1]->getQuantity() > 0){ //checks if the book is in stock
+					tempV[answ - 1]->setQuantity(tempV[0]->getQuantity() - 1); //Reduces stock by one
+					titles[items] = tempV[answ - 1]->getBookTitle(); //stores titles for receipt
+					prices[items] = tempV[answ - 1]->getRetailCost(); //stores prices for receipt
+					subTotal += tempV[answ - 1]->getRetailCost(); //counter items increase by one
 					items++;
-					cout << endl << "$" << fixed << setprecision(2) << prices[items-1] << "     " << titles[items-1] << endl << endl;
+					cout << endl << "$" << fixed << setprecision(2) << prices[items - 1] << "     " << titles[items - 1] << endl << endl;
 				}
 				else{
 					cout << "Out of Stock" << endl << endl;
@@ -227,12 +226,11 @@ void CashierModule::titleCheckOut(){
 				while (!readInt(answ) || (answ < 1) || (answ >tempV.size())) { //checks if user selection is valid. If not, user must re-input selection
 					cout << "\nPlease enter a number between 1 and " << tempV.size() << ": ";
 				}
-				if (tempV[0]->getQuantity() > 0){ //checks if the book is in stock
-					tempV[0]->setQuantity(tempV[0]->getQuantity() - 1); //Reduces stock by one
-					titles[items] = tempV[0]->getBookTitle(); //stores titles for receipt
-					prices[items] = tempV[0]->getRetailCost(); //stores prices for receipt
-					stock[items] = tempV[0]->getQuantity(); //increments subtotal
-					subTotal += tempV[0]->getRetailCost(); //counter items increase by one
+				if (tempV[answ - 1]->getQuantity() > 0){ //checks if the book is in stock
+					tempV[answ - 1]->setQuantity(tempV[0]->getQuantity() - 1); //Reduces stock by one
+					titles[items] = tempV[answ - 1]->getBookTitle(); //stores titles for receipt
+					prices[items] = tempV[answ - 1]->getRetailCost(); //stores prices for receipt
+					subTotal += tempV[answ - 1]->getRetailCost(); //counter items increase by one
 					items++;
 					cout << endl << "$" << fixed << setprecision(2) << prices[items - 1] << "     " << titles[items - 1] << endl << endl;
 				}
@@ -282,8 +280,8 @@ void CashierModule::printReceipt(){
 	if (system("CLS")) system("clear");
 	cout << setw(30) << "Order Number 000" << orderNumber - 1 << endl << endl;
 	for (int i = 0; i < items; i++){
-		cout << titles[i + 1] << endl;
-		cout << "                             $" << fixed << setprecision(2) << prices[i + 1] << endl << endl;
+		cout << titles[i] << endl;
+		cout << "                             $" << fixed << setprecision(2) << prices[i] << endl << endl;
 	}
 	cout << "-------------------------------------------------" << endl;
 	cout << "                             Subtotal: " << subTotal << endl;
